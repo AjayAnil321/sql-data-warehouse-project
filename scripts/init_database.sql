8a88949ce23723882c47f2b4520c7290a3adfb5b
Ajay Anil <162934855+AjayAnil321@users.noreply.github.com>
2025-11-04 15:59:38 +0530

    Create init_database.sql

---
+++ scripts/init_database.sql
@@ -0,0 +1,17 @@
+-- create Database 'Data Warehouse'--
+
+USE master;
+GO
+
+CREATE DATABASE DataWareHouse;
+
+USE DataWareHouse;
+
+CREATE SCHEMA bronze;
+GO
+
+CREATE SCHEMA silver;
+GO
+
+CREATE SCHEMA gold;
+GO
